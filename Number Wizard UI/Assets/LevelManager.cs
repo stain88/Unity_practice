2098cceae981a415328474c83dfb71b69855aab6
stain88 <marcbaghdadi@gmail.com>
2016-05-03 20:51:30 +0100

    Number Wizard UI lecture 46

---
+++ Number Wizard UI/Assets/LevelManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		Debug.Log ("Level load requested for: " + name);
+	}
+
+	public void QuitRequest() {
+		Debug.Log ("I want to quit!");
+	}
+
+}
